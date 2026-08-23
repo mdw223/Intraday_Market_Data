02d8e3ed30c13371fc4b025152c6a2b8e7e1c9c7
mdw223 <malik.code@proton.me>
2026-08-23 19:47:27 -0400

    added logging

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,18 @@
 import "dotenv/config"; // load environment variables from .env file
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import router from "./routes/routes";
 import { errorHandler } from "./middleware/errorHandler";
 const port = process.env.PORT || 3000;
 const app = express(); // create express application
 
 // Restricted to only the allowed origin, the frontend URL
-app.use(cors({ origin: process.env.ALLOWED_ORIGIN })); // enable cors
-app.use(express.json()); // parse json bodies
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+app.use(express.json());
+// log requests in development mode
+// combined is more verbose for production
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev")); 
 
 // define routes
 app.use("/api", router);
